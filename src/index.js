b214f445387e9b994433078193b9c8d56d5558d9
Kadavic <adriangezequiel@gmail.com>
2023-02-07 20:44:19 -0300

    node_modules

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import fs from "fs"
 import { v4 as uuidv4 } from 'uuid'
 
+
 class ProductManager {
   constructor(path){
     this.products = []
     this.path = path
   }
-  checkDB(){
+  async checkDB(){
     if(fs.existsSync(this.path)){
-      this.products = JSON.parse(fs.readFileSync(this.path))
+      this.products = JSON.parse(await fs.promises.readFile(this.path))
     }
   }
-  addProduct(title, description, price, thumbnail, code, stock){
-    this.checkDB()
+  async updateDB(){
+    await fs.promises.writeFile(this.path, JSON.stringify(this.products))
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock){
+    await this.checkDB()
     const isInArray = this.products.some(product => product.code === code)
     if(isInArray === false && title && description && price && thumbnail && stock){
       this.products.push({
@@ -24,17 +29,18 @@ class ProductManager {
         code: code,
         stock: stock
       });
-      fs.writeFileSync(this.path, JSON.stringify(this.products))
+      this.updateDB
       return "Producto agregado"
     }else{
       return "Producto repetido"
     }
   }
-  getProducts(){
+  async getProducts(){
+    await this.checkDB()
     return this.products
   }
-  getProductById(id){
-    this.checkDB()
+  async getProductById(id){
+    await this.checkDB()
     const productFound = this.products.find(product => product.id === id)
     if (productFound){
       return productFound
@@ -42,8 +48,8 @@ class ProductManager {
       return "No se encuentra el producto"
     }
   }
-  updateProduct(id, title, description, price, thumbnail, code, stock){
-    this.checkDB()
+  async updateProduct(id, title, description, price, thumbnail, code, stock){
+    await this.checkDB()
     const indexFound = this.products.findIndex(product => product.id === id)
     if(indexFound !== -1){
       this.products[indexFound] = {
@@ -55,36 +61,40 @@ class ProductManager {
         code: code,
         stock: stock
       }
-      fs.writeFileSync(this.path, JSON.stringify(this.products))
+      await this.updateDB()
       return "Producto actualizado"
     }else{
       return "No se encuentra el producto"
     }
   }
-  deleteProduct(id){
-    this.checkDB()
+  async deleteProduct(id){
+    await this.checkDB()
     const indexFound = this.products.findIndex(product => product.id === id)
     if(indexFound !== -1){
       this.products.splice(indexFound,indexFound+1)
-      fs.writeFileSync(this.path, JSON.stringify(this.products))
+      await this.updateDB()
       return "Producto eliminado"
     }else{
       return "No se encuentra el producto"
     }
   }
 }
-const productManager = new ProductManager("./products.json")
 
-console.log(productManager.addProduct("producto 1","descripción", 100, "Sin imagen","001",12))
-console.log(productManager.addProduct("producto 2","descripción", 100, "Sin imagen","002",12))
-console.log(productManager.addProduct("producto 3","descripción", 100, "Sin imagen","003",12))
-console.log(productManager.addProduct("producto 4","descripción", 100, "Sin imagen","004",12))
-console.log(productManager.getProducts())
+export const productManager = new ProductManager("./src/products.json")
+
+productManager.addProduct("producto 1","descripción", 100, "Sin imagen","001",12)
+productManager.addProduct("producto 2","descripción", 100, "Sin imagen","002",12)
+productManager.addProduct("producto 3","descripción", 100, "Sin imagen","003",12)
+productManager.addProduct("producto 4","descripción", 100, "Sin imagen","004",12)
+productManager.addProduct("producto 5","caja", 123, "Sin imagen","005",12).then(val => console.log(val))
+productManager.getProducts().then(val => console.log(val))
+
 
 //Quitar comentario a la siguiente linea y colocar un id
 //console.log(productManager.getProductById("6e30eaf8-2936-414b-9627-d9687342353d"))
 //Quitar comentario a las dos lineas que siguen para ver que actualice el producto y verlo
-console.log(productManager.updateProduct("2f090b05-d3fd-4c60-a1d5-611191390c02", "producto 1 actualizado","descripción actualizada", 100, "Sin imagen","001",12))
+//console.log(productManager.updateProduct("2f090b05-d3fd-4c60-a1d5-611191390c02", "producto 1 actualizado","descripción actualizada", 100, "Sin imagen","001",12))
 //console.log(productManager.getProducts())
 //Quitar comentario a la siguiente linea para ver como se elimina el producto
-console.log(productManager.deleteProduct("cbfe5e84-813e-42be-a954-86c7cbe3c5bc"))
+//console.log(productManager.deleteProduct("5d020c89-d6d8-46e1-ad48-d34b301d8326"))
+
